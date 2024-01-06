06b218de419b3d8814754069eeb305298cbf6e38
divyayush-v-20 <122596172+divyayush-v-20@users.noreply.github.com>
2024-01-06 22:18:51 +0530

    Update 515 Find Largest Value in Each Tree Row.cpp

--- Leetcode/515 Find Largest Value in Each Tree Row.cpp
+++ Leetcode/515 Find Largest Value in Each Tree Row.cpp
@@ -12,27 +12,22 @@
 class Solution {
 public:
     vector<int> largestValues(TreeNode* root) {
-        vector<int> max;
-        if(root==NULL) return max;
+        vector<int> maxV;
+        if(root==NULL) return maxV;
         queue<TreeNode*> q;
         q.push(root);
         while(!q.empty()){
             int n = q.size();
-            stack<int> s;
-            s.push(INT_MIN);
+            int m = INT_MIN;
             for(int i=0;i<n;i++){
                 TreeNode *f = q.front();
                 q.pop();
-                if(f->val >= s.top()){
-                    s.pop();
-                    s.push(f->val);
-                }
+                m = max(m,f->val);
                 if(f->left) q.push(f->left);
                 if(f->right) q.push(f->right);
             }
-            max.push_back(s.top());
-            s.pop();
+            maxV.push_back(m);
         }
-        return max;
+        return maxV;
     }
 };
