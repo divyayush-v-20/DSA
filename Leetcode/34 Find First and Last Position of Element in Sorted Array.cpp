2a713996c9f61bf90785e2161682678b2ea78de2
divyayush-v-20 <122596172+divyayush-v-20@users.noreply.github.com>
2024-01-10 21:51:29 +0530

    Update 34 Find First and Last Position of Element in Sorted Array.cpp

--- Leetcode/34 Find First and Last Position of Element in Sorted Array.cpp
+++ Leetcode/34 Find First and Last Position of Element in Sorted Array.cpp
@@ -1,21 +1,46 @@
 class Solution {
 public:
+    int firstOcc(vector<int>& nums,int t){
+        int ans = -1;
+        int l = 0,r = nums.size()-1,m;
+        while(l<=r){
+            m=l+(r-l)/2;
+            if(nums[m]==t){
+                ans = m;
+                r = m-1;
+            }
+            else if(nums[m]>t){
+                r=m-1;
+            }
+            else{
+                l=m+1;
+            }
+        }
+        return ans;
+    }
+    int lastOcc(vector<int>& nums,int t){
+        int ans = -1;
+        int l = 0,r = nums.size()-1,m;
+        while(l<=r){
+            m=l+(r-l)/2;
+            if(nums[m]==t){
+                ans = m;
+                l=m+1;
+            }
+            else if(nums[m]>t){
+                r=m-1;
+            }
+            else{
+                l=m+1;
+            }
+        }
+        return ans;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        pair<int,int> p;
-        p.first = -1;
-        p.second = -1;
+        pair<int,int> p = {-1,-1};
         vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                p.first = i;
-                break;
-            }
-        }
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                p.second = i;
-            }
-        }
+        p.first = firstOcc(nums,target);
+        p.second = lastOcc(nums,target);
         ans.push_back(p.first);
         ans.push_back(p.second);
         return ans;
