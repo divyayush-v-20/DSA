fd68a7d65134e3d984bb47b0b37bd91c178e7ffd
divyayush-v-20 <122596172+divyayush-v-20@users.noreply.github.com>
2024-02-15 21:07:36 +0530

    Update 46 Permutations.cpp

--- Leetcode/46 Permutations.cpp
+++ Leetcode/46 Permutations.cpp
@@ -15,12 +15,25 @@ public:
             }
         }
     }
+    void optimizedSolver(vector<int> nums,vector<vector<int>>& ans,int l,int r){
+        if(l==r){
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=l;i<=r;i++){
+            swap(nums[l],nums[i]);
+            optimizedSolver(nums,ans,l+1,r);
+            swap(nums[l],nums[i]);
+        }
+    }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        int freq[nums.size()];
-        vector<int> tmp;
-        for(int i=0;i<nums.size();i++) freq[i] = 0;
-        solve(nums,ans,tmp,freq);
+        // int freq[nums.size()];
+        // vector<int> tmp;
+        // for(int i=0;i<nums.size();i++) freq[i] = 0;
+        // solve(nums,ans,tmp,freq);
+        int n = nums.size();
+        optimizedSolver(nums,ans,0,n-1);
         return ans;
     }
 };
