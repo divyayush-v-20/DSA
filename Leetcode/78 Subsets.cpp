a5ea5959ad35c20aafa0df8fc83df61ab01099a7
divyayush-v-20 <122596172+divyayush-v-20@users.noreply.github.com>
2024-02-15 21:33:19 +0530

    Update 78 Subsets.cpp

--- Leetcode/78 Subsets.cpp
+++ Leetcode/78 Subsets.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    void solve(vector<int>& nums,set<vector<int>>&s,vector<int> tmp,int i){
+    void solve(vector<int>& nums,vector<vector<int>>&ans,vector<int> tmp,int i){
         if(i==nums.size()){
-            s.insert(tmp);
+            ans.push_back(tmp);
             return;
         }
         tmp.push_back(nums[i]);
-        solve(nums,s,tmp,i+1);
+        solve(nums,ans,tmp,i+1);
         tmp.pop_back();
-        solve(nums,s,tmp,i+1);
+        solve(nums,ans,tmp,i+1);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        set<vector<int>> st;
+        // set<vector<int>> st;
         vector<int> tmp;
-        solve(nums,st,tmp,0);
         vector<vector<int>> ret;
-        for(auto v:st){
-            ret.push_back(v);
-        }
+        solve(nums,ret,tmp,0);
+        // for(auto v:st){
+        //     ret.push_back(v);
+        // }
         return ret;
     }
 };
