1b9d0f0c9fc37833d479d70fc5d483e83fb311a1
divyayush-v-20 <122596172+divyayush-v-20@users.noreply.github.com>
2024-04-29 22:41:56 +0530

    Create 70 Climbing Stairs.cpp

---
+++ Leetcode/70 Climbing Stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+//using memoization
+    int memo(int n,vector<int>& dp){
+        if(n==1 || n==2){
+            return n;
+        } 
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n] = memo(n-1,dp) + memo(n-2,dp);
+    }
+    int climbStairs(int n) {
+        if(n==0) return 0;
+        vector<int> dp(n+1,-1);
+        return memo(n,dp);
+    }
+};
