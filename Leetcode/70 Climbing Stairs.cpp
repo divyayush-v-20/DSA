f598a7e9d5cd7cd9a6b9105eca75f458b9900d12
divyayush-v-20 <122596172+divyayush-v-20@users.noreply.github.com>
2024-05-01 10:31:15 +0530

    Update 70 Climbing Stairs.cpp

--- Leetcode/70 Climbing Stairs.cpp
+++ Leetcode/70 Climbing Stairs.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-//using memoization
+    //memoization
     int memo(int n,vector<int>& dp){
         if(n==1 || n==2){
             return n;
@@ -10,9 +10,22 @@ public:
         }
         return dp[n] = memo(n-1,dp) + memo(n-2,dp);
     }
+    //tabulation
+    int tab(int n){
+        if(n==1) return 1;
+        if(n==2) return 2;
+        int p1 = 1,p2 = 2,ans;
+        for(int i=3;i<=n;i++){
+            ans = p1+p2;
+            p1 = p2;
+            p2 = ans;
+        }
+        return p2;
+    }
     int climbStairs(int n) {
         if(n==0) return 0;
-        vector<int> dp(n+1,-1);
-        return memo(n,dp);
+        // vector<int> dp(n+1,-1);
+        // return memo(n,dp);
+        return tab(n);
     }
 };
