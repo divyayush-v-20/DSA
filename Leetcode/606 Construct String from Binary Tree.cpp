56ec29cd4e12ff2631bcbaf1aeb1761eda35f0c1
Divyayush Verma <122596172+divyayush-v-20@users.noreply.github.com>
2024-05-05 00:28:02 +0530

    Create 606 Construct String from Binary Tree.cpp

---
+++ Leetcode/606 Construct String from Binary Tree.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+        string str = "";
+        if(root==NULL) return str;
+        helper(root,str);
+        return str;
+    }
+    void helper(TreeNode* root,string& str){
+        if(root==NULL) return;
+
+        str += to_string(root->val);
+
+        if(root->left){
+            str+="(";
+            helper(root->left,str);
+            str+=")";
+        }
+        if(!root->left && root->right){
+            str+="()";
+        }
+        if(root->right){
+            str+="(";
+            helper(root->right,str);
+            str+=")";
+        }
+    }
+};
