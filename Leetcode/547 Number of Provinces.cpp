066bf186cedb31cd8d617b2a8bd2a84538b9c5d8
Divyayush Verma <122596172+divyayush-v-20@users.noreply.github.com>
2024-05-25 23:02:10 +0530

    Update 547 Number of Provinces.cpp

--- Leetcode/547 Number of Provinces.cpp
+++ Leetcode/547 Number of Provinces.cpp
@@ -8,6 +8,20 @@ public:
             }
         }
     }
+    void bfs(int node,vector<vector<int>>& mat,vector<int>& vis){
+        queue<int> q;
+        q.push(node);
+        while(!q.empty()){
+            int cur = q.front();
+            q.pop();
+            for(int i=0;i<mat.size();i++){
+                if(!vis[i] && mat[cur][i]){
+                    q.push(i);
+                    vis[i] = 1;
+                }
+            }
+        }
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int ans = 0;
         int V = isConnected.size();
@@ -15,7 +29,8 @@ public:
         for(int i=0;i<V;i++){
             if(!vis[i]){
                 ans++;
-                dfs(i,isConnected,vis);
+                //dfs(i,isConnected,vis);
+                bfs(i,isConnected,vis);
             }
         }
         return ans;
